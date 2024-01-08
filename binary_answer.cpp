51988919abbbf4011fe6dd477ac61ca71ea7f75d
Ginatia <feishiforever@163.com>
2024-01-08 15:09:11 +0800

    Create binary_answer.cpp

---
+++ binary_answer.cpp
@@ -0,0 +1,38 @@
+#include <functional>
+template <typename T>
+T first_true(T l, T r, std::function<bool(T)> &&f)
+{
+    r++;
+    while (l < r)
+    {
+        T mid = l + (r - l) / 2;
+        if (f(mid))
+        {
+            r = mid;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    return l;
+}
+
+template <typename T>
+T last_true(T l, T r, std::function<bool(T)> &&f)
+{
+    l--;
+    while (l < r)
+    {
+        T mid = l + (r - l + 1) / 2;
+        if (f(mid))
+        {
+            l = mid;
+        }
+        else
+        {
+            r = mid - 1;
+        }
+    }
+    return l;
+}
