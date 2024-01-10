6c918878271906981c254d37f6a0900314219994
Ginatia <feishiforever@163.com>
2024-01-10 17:46:13 +0800

    main

--- python/dijkstra.py
+++ python/dijkstra.py
@@ -1,36 +1,37 @@
 import heapq
 
 
-def solve():
-    n, m = map(int, input().split())
-    G = [[] for _ in range(n)]
-    for _ in range(m):
-        a, b, c = map(int, input().split())
-        a -= 1
-        b -= 1
-        G[a].append((b, c))
-        G[b].append((a, c))
+class Dijkstra:
 
-    def dist(s: int):
-        pq = []
-        INF = 10**9
+    def __init__(self, _n: int) -> None:
+        self.G = [[] for _ in range(_n)]
+        self.bs = [-1]*_n
+
+    def add_edge(self, u: int, v: int, c):
+        self.G[u].append((v, c))
+
+    def build(self, s: int, INF):
+        n = len(self.G)
         ds = [INF]*n
         ds[s] = 0
+        pq = []
         heapq.heappush(pq, (0, s))
         while len(pq) > 0:
             cost_u, u = heapq.heappop(pq)
             if ds[u] < cost_u:
                 continue
-            for v, cost_v in G[u]:
+            for v, cost_v in self.G[u]:
                 if ds[v] > ds[u]+cost_v:
                     ds[v] = ds[u]+cost_v
+                    self.bs[v] = u
                     heapq.heappush(pq, (ds[v], v))
         return ds
 
-    d1 = dist(0)
-    dn = dist(n-1)
-    for i in range(n):
-        print(d1[i]+dn[i])
-
-
-solve()
+    def restore(self, to: int):
+        ans = []
+        if self.bs[to] < 0:
+            return ans
+        while ~to:
+            ans.append(to)
+            to = self.bs[to]
+        return ans[::-1]
